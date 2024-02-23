925762efebb34886ced3fbc6fac487eccdd1450d
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-02-23 19:57:45 -0300

    [create] get user profile use case & test

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,11 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { Prisma, User } from '@prisma/client'
 
 import { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
+
+  async findById(userId: string): Promise<User | null> {
+    const user = this.items.find((item) => item.id === userId)
+
+    if (!user) return null
+
+    return user
+  }
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email)
