d31bab6325f0dd736641a39e0f95c473117f71d0
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-03-10 18:27:45 -0300

    add authenticate e2e test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,32 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+import { app } from '@/app'
+
+describe('Authenticate E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@mail.com',
+      password: 'password',
+    })
+
+    const authRes = await request(app.server).post('/sessions').send({
+      email: 'john.doe@mail.com',
+      password: 'password',
+    })
+
+    expect(authRes.statusCode).toBe(200)
+    expect(authRes.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
