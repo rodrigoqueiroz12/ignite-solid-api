ad92fe810279e495a917b553096b01c798966765
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-03-10 18:10:02 -0300

    add register e2e test

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,24 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+import { app } from '@/app'
+
+describe('Register E2E', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const res = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@mail.com',
+      password: 'password',
+    })
+
+    expect(res.statusCode).toBe(201)
+  })
+})
