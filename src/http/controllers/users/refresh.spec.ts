eb1601cf4ca0fab5bf06e5630f35234ca180ce66
Rodrigo Rocha de Queiroz <rodrigo@emperium.com.br>
2024-03-11 19:39:27 -0300

    create refresh token revalidate & tests

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
+
+describe('Authenticate E2E', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh the token', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@mail.com',
+      password: 'password',
+    })
+
+    const authRes = await request(app.server).post('/sessions').send({
+      email: 'john.doe@mail.com',
+      password: 'password',
+    })
+
+    const cookies = authRes.get('Set-Cookie')
+
+    const res = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(res.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { me } from './me'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /* Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, me)
